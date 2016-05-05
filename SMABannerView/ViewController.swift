ea8f9f07dc76297b4e105cca21eeeee85a7cd826
Bill Burgess <bill@simplymadeapps.com>
2016-05-05 08:20:38 -0500

    Initial project commit

---
+++ SMABannerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SMABannerView
+//
+//  Created by Bill Burgess on 5/5/16.
+//  Copyright © 2016 Simply Made Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
